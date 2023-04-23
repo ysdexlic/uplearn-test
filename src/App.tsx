892f338160ba5d747e3f7d0efb0b9ed67a622870
David Thompson <david.arthur.thompson@gmail.com>
2023-04-23 11:34:42 +0100

    add apollo and query to spacex

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
-function App() {
+const gqlClient = new ApolloClient({
+  uri: 'https://spacex-production.up.railway.app/',
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
+  const [launches, setLaunches] = useState({})
+
+  useEffect(() => {
+    const fetchLaunches = async () => {
+      const res = await gqlClient
+      .query({
+        query: gql`
+        query Launches {
+          launches {
+            id
+            details
+            mission_name
+            links {
+              video_link
+              article_link
+            }
+            launch_date_utc
+          }
+        }
+        `,
+      })
+      setLaunches(res)
+    }
+    fetchLaunches()
+  }, [setLaunches])
+
+  console.log(launches)
+
   return (
     <div className="App">
       <header className="App-header">
